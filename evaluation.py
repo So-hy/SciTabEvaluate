00b70be3f4b923c614679e744d698c0449f92193
So-hy <168037454+So-hy@users.noreply.github.com>
2024-09-17 22:31:13 +0900

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -2,14 +2,9 @@
 
 from sklearn.metrics import classification_report, f1_score
 
-def calculate_metrics(true_labels, pred_labels, class_labels, file=None):
+def calculate_metrics(true_labels, pred_labels, labels):
     """실제 레이블과 예측 레이블을 비교하여 성능 지표를 계산합니다."""
-    report = classification_report(true_labels, pred_labels, labels=class_labels, digits=4)
-    macro_f1 = f1_score(true_labels, pred_labels, average='macro', labels=class_labels)
-
-    if file:
-        file.write(report)
-        file.write(f'\nMacro-F1 Score: {macro_f1}\n')
-    else:
-        print(report)
-        print('Macro-F1 Score:', macro_f1)
+    report = classification_report(true_labels, pred_labels, labels=labels, digits=4)
+    macro_f1 = f1_score(true_labels, pred_labels, average='macro', labels=labels)
+    print(report)
+    print('Macro-F1 Score:', macro_f1)
